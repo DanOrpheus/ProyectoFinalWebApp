624f6541d2577e1a75c251683ca0293806c14113
NadiaAlejandraSotoLopez <nadia.soto210930@potros.itson.edu.mx>
2023-06-12 18:47:51 -0700

    Métodos en municipio
    Agregue métodos get y set en la clase Municipio

--- src/main/java/org/itson/dominio/Municipio.java
+++ src/main/java/org/itson/dominio/Municipio.java
@@ -9,5 +9,29 @@ package org.itson.dominio;
  * @version IDE 17
  */
 public class Municipio {
-    
+    private int id;
+    private String nombre;
+
+    // Constructor
+    public Municipio(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    // Métodos getters y setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }

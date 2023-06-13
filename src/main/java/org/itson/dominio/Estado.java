1c00d472c95c88131da81441561216c0ddf9ca62
NadiaAlejandraSotoLopez <nadia.soto210930@potros.itson.edu.mx>
2023-06-12 19:22:54 -0700

    Estado
    Métodos get y set

--- src/main/java/org/itson/dominio/Estado.java
+++ src/main/java/org/itson/dominio/Estado.java
@@ -3,11 +3,36 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package org.itson.dominio;
-
+import org.bson.types.ObjectId;
 /**
  * @author Victor, Victoria, Daniel y Nadia
  * @version IDE 17
  */
 public class Estado {
+    private ObjectId id;
+    private String nombre;
     
+    // Constructor
+    public Estado(ObjectId id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+    
+    // Métodos getters y setters
+    public ObjectId getId() {
+        return id;
+    }
+    
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
+

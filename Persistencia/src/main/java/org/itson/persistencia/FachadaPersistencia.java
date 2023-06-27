cc702fdb3e9e7d76bdacfa7f50bd9cd10ff637ac
Antonio <victorglezb@gmail.com>
2023-06-26 18:37:41 -0700

    Dividí los proyectos individualmente
    Un proyecto de dominio, y uno de persistencia que tiene como dependencia el de dominio. Para utilizarlos como dependencias en el servidor web final.

--- Persistencia/src/main/java/org/itson/persistencia/FachadaPersistencia.java
+++ Persistencia/src/main/java/org/itson/persistencia/FachadaPersistencia.java
@@ -20,85 +20,92 @@ import org.itson.persistencia.DAO.UsuariosDAO;
  * @version IDE 18
  */
 public class FachadaPersistencia {
+    // ATRIBUTOS DE LA CLASE
+    private UsuariosDAO usDAO;
+    private PostsDAO posDAO;
+    private AdminsDAO adDAO;
+    private AncladosDAO ancDAO;
+    private ComentariosDAO comDAO;
+    
     // MÉTODOS
     /**
      * Método que guarda un usuario en la base de datos
      * @param usuario 
+     * @return  
      */
-    public void guardarUsuario(Usuario usuario){
-        UsuariosDAO usuariosDAO=new UsuariosDAO();
-        usuariosDAO.agregar(usuario);
+    public Usuario guardarUsuario(Usuario usuario){
+        return this.usDAO.agregar(usuario);
     }
     /**
      * Método que guarda un post en la base de datos
      * @param post 
+     * @return  
      */
-    public void guardarPost(Post post){
-        PostsDAO postsDAO=new PostsDAO();
-        postsDAO.agregar(post);
+    public Post guardarPost(Post post){
+        return this.posDAO.agregar(post);
     }
     /**
      * Método que guarda un admin en la base de datos
      * @param admin 
+     * @return  
      */
-    public void guardarAdmin(Admin admin){
-        AdminsDAO adminsDAO=new AdminsDAO();
-        adminsDAO.agregar(admin);
+    public Admin guardarAdmin(Admin admin){
+        return this.adDAO.agregar(admin);
     }
     /**
      * Método que guarda un post anclado en la base de datos
      * @param anclado 
+     * @return  
      */
-    public void guardarAnclados(Anclado anclado){
-        AncladosDAO ancladosDAO=new AncladosDAO();
-        ancladosDAO.agregar(anclado);
+    public Anclado guardarAnclados(Anclado anclado){
+        return this.ancDAO.agregar(anclado);
     }
     /**
      * Método que guarda un comentario en la base de datos
      * @param comentario 
+     * @return  
      */
-    public void guardarComentario(Comentario comentario){
-        ComentariosDAO comentariosDAO=new ComentariosDAO();
-        comentariosDAO.agregar(comentario);
+    public Comentario guardarComentario(Comentario comentario){
+        return this.comDAO.agregar(comentario);
     }
     /**
      * Método que modifica un post de la base de datos
      * @param post 
+     * @return  
      */
-    public void modificarPost(Post post){
-        PostsDAO postsDAO=new PostsDAO();
-        postsDAO.modificar(post);
+    public Post modificarPost(Post post){
+        return this.posDAO.modificar(post);
     }
     /**
      * Método que modifica un anclado de la base de datos
      * @param anclado 
+     * @return  
      */
-    public void modificarAnclado(Anclado anclado){
-        AncladosDAO ancladosDAO=new AncladosDAO();
-        ancladosDAO.modificar(anclado);
+    public Anclado modificarAnclado(Anclado anclado){
+        return this.ancDAO.modificar(anclado);
     }
     /**
      * Método que elimina un post de la base de datos
      * @param post 
+     * @return  
      */
-    public void eliminarPost(Post post){
-        PostsDAO postsDAO=new PostsDAO();
-        postsDAO.eliminar(post);
+    public Post eliminarPost(Post post){
+        return this.posDAO.eliminar(post);
     }
     /**
      * Método que elimina un post anclado de la base de datos
      * @param anclado 
+     * @return  
      */
-    public void eliminarAnclado(Anclado anclado){
-        AncladosDAO ancladosDAO=new AncladosDAO();
-        ancladosDAO.eliminar(anclado);
+    public Anclado eliminarAnclado(Anclado anclado){
+        return this.ancDAO.eliminar(anclado);
     }
     /**
      * Método que elimina un comentario de la base de datos
      * @param comentario 
+     * @return  
      */
-    public void eliminarComentario(Comentario comentario){
-        ComentariosDAO comentariosDAO=new ComentariosDAO();
-        comentariosDAO.eliminar(comentario);
+    public Comentario eliminarComentario(Comentario comentario){
+        return this.comDAO.eliminar(comentario);
     }
 }

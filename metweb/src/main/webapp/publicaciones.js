11b3c289371fe37b2f21450c562ca1bec8f04436
NadiaAlejandraSotoLopez <nadia.soto210930@potros.itson.edu.mx>
2023-07-06 00:42:37 -0700

    Guardar publicación FetchAPI
    Código para guardar una publicación con FetchAPI

--- metweb/src/main/webapp/publicaciones.js
+++ metweb/src/main/webapp/publicaciones.js
@@ -60,7 +60,39 @@ window.onload = function () {
         modal.style.display = "none";
     }
 
+//Guardar publicación con FetchAPI
+document.getElementById("form_publicaciones").addEventListener("submit", function(event) {
+  event.preventDefault(); // Evita que se envíe el formulario automáticamente
 
+  // Obtiene los valores del formulario
+  var title = document.getElementById("title").value;
+  var content = document.getElementById("content").value;
+
+  // Crea objeto con los datos de la publicación
+  var publicacion = {
+    title: title,
+    content: content
+  };
+
+  // Solicitud POST al servidor para guardar la publicación
+  fetch("/guardar-publicacion", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(publicacion)//Se convierte el objeto en JSON
+  })
+  .then(function(response) {
+    if (response.ok) {
+      console.log("Publicación guardada exitosamente");
+    } else {
+      console.log("Error al guardar la publicación");
+    }
+  })
+  .catch(function(error) {
+    console.log("Error de red:", error);
+  });
+});
 
     // ASIGNACIÓN SEMÁNTICA
     const btnPublicar = document.getElementById("btn-publicar");

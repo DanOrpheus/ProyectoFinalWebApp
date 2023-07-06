fd0fc6c3d2d7e2f29b6b9554e764c2854d2e1455
Antonio <victorglezb@gmail.com>
2023-07-05 20:37:10 -0700

    JS para Publicaciones y Comentarios
    Los métodos de guardarPublicacion y guardarComentario creados en archivos javascript. Falta agregar un formulario para los comentarios y hacer que se muestren las publicaciones

--- metweb/src/main/webapp/comentarios.js
+++ metweb/src/main/webapp/comentarios.js
@@ -1,22 +1,20 @@
 window.onload = function(){
     // GUARDAR DATOS
-    const guardarPublicacion = () => {
+    const guardarComentario = () => {
         const btnPublicar = document.getElementById("btn-publicar");
         btnPublicar.disabled = true;
-        // Obtener los datos del form de publicaciones
-        const titulo = document.getElementById("title");
+        // Obtener los datos del form de comentarios
         const contenido = document.getElementById("content").value;
         const fechaHoraCreacion = new Date();
-        const publicacion = {
-            titulo,
+        const comentario = {
             contenido,
             fechaHoraCreacion
         };
         // Enviar datos al server con FetchAPI
         fetch({
-            url: "http://localhost:8080/metweb/posts?action=create",
+            url: "http://localhost:8080/metweb/comments?action=create",
             method: "POST",
-            body: publicacion
+            body: comentario
         }).then(response => {
             return response.json();
         }).then(response => {
@@ -28,5 +26,5 @@ window.onload = function(){
 
     // ASIGNACIÓN SEMÁNTICA
     const btnPublicar = document.getElementById("btn-publicar");
-    btnPublicar.onclick = guardarPublicacion;
+    btnPublicar.onclick = guardarPublicacion;    
 };
